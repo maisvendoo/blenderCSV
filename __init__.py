04015a69ebc7937fc3f16e43359c7aecb6c56bf9
maisvendoo <maisvendoo@gmail.com>
2018-07-10 00:13:49 +0300

    Удалось создать четыре вершины в окне блендера

--- __init__.py
+++ __init__.py
@@ -1,39 +1,61 @@
-
-
-
-
-
+#-----------------------------------------------------------------------------------------------------------------------
+#
+#       Plugin to import OpenBVE CSV models into Blender 3D editor
+#       RGUPS, Virtual Railway 09/07/2018
+#       Developer: Dmitry Pritykin
+#
+#-----------------------------------------------------------------------------------------------------------------------
 bl_info = {
-    "name": "OpenBVE CSV format",
+    "name": "Importer OpenBVE CSV models",
+    "category": "Import-Export",
     "author": "Dmitry Pritykin",
     "version": (0, 1, 0),
-    "blender": (2, 78, 0),
-    "location": "File > Import-Export",
-    "description": "Import CSV mesh, UV's, materials and textures",
-    "warning": "",
-    "wiki_url": "https://vk.com/maisvendoo",
-    "support": 'OFFICIAL',
-    "category": "Import-Export"}
-	
+    "blender": (2, 78, 0)
+}
+
 import bpy
+import bmesh
 
 class CSVImporter(bpy.types.Operator):
-	"""Load CSV mesh data"""
-	bl_idname = "import_mesh.csv"
-	bl_label = "Import CSV"
-	bl_options = {'UNDO'}
-	
-	def execute(self, context):
-		return {'FINISHED'}
+    """CSV models importer"""
+    bl_idname = "import_scene.csv"
+    bl_label = "OpenBVE CSV model (*.csv)"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+
+        verts = [(1, 1, 0), (-1, 1, 0), (-1, -1, 0), (1, -1, 0)]
+        mesh = bpy.data.meshes.new("square")
+        obj = bpy.data.objects.new("Square", mesh)
+
+        scene = bpy.context.scene
+        scene.objects.link(obj)
+        scene.objects.active = obj
+        obj.select = True
+
+        mesh = bpy.context.object.data
+        bm = bmesh.new()
+
+        for v in verts:
+            bm.verts.new(v)
+
+        bm.to_mesh(mesh)
+        bm.free()
+
+        return {'FINISHED'}
 
 def menu_import(self, context):
-	self.layout.operator(CSVImporter.bl_idname, text="OpenBVE CSV (.csv)")
-	
+    self.layout.operator(CSVImporter.bl_idname, text=CSVImporter.bl_label)
+
 def register():
-	bpy.types.INFO_MT_file_import.append(menu_import)
-	
+    bpy.types.INFO_MT_file_import.append(menu_import)
+    bpy.utils.register_class(CSVImporter)
+
+
 def unregister():
-	bpy.types.INFO_MT_file_import.remove(menu_import)
+    bpy.types.INFO_MT_file_import.remove(menu_import)
+    bpy.utils.register_class(CSVImporter)
+
 
 if __name__ == "__main__":
-	register()
+    register()
