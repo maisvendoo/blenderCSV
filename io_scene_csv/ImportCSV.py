ccc58e3d95cadcd7bea3eb271659b622fd049fc1
maisvendoo <maisvendoo@gmail.com>
2018-07-26 08:52:20 +0300

    Импорт CSV вынесен в отдельный модуль

---
+++ io_scene_csv/ImportCSV.py
@@ -0,0 +1,161 @@
+#-------------------------------------------------------------------------------
+#
+#       CSV models import module
+#       (c) RGUPS, Virtual Railway 26/07/2018
+#       Developer: Dmitry Pritykin
+#
+#-------------------------------------------------------------------------------
+import bpy
+import bmesh
+import os
+
+#-------------------------------------------------------------------------------
+#
+#-------------------------------------------------------------------------------
+class ImportCSV:
+
+    def __init__(self):
+        self.filepath = ""
+        pass
+
+    # ---------------------------------------------------------------------------
+    #
+    # ---------------------------------------------------------------------------
+    def getFileName(self, path):
+        basename = os.path.basename(path)
+        tmp = basename.split(".")
+        return tmp[0]
+
+    # ---------------------------------------------------------------------------
+    #
+    # ---------------------------------------------------------------------------
+    def createMaterial(self, md, mesh):
+
+        # Создаем материал. Если есть текстура, то используем имя
+        # файла текстуры в качестве имени материала
+        if mesh.texture_file != "":
+            matName = self.getFileName(mesh.texture_file)
+        else:
+            matName = md.name;
+
+        print("Creation of material: ", matName)
+
+        # Проверяем, существует ли уже такой материал
+        mat = bpy.data.materials.get(matName)
+
+        # Если не существует, создаем его заново
+        if mat is None:
+            mat = bpy.data.materials.new(name=matName)
+            print("New material ", matName, " created")
+        else:
+            print("Material ", matName, " already exists")
+            # проверяем число текстурных слотов в материале
+            if len(mat.texture_slots) == 18:
+                print("WARNING: maximal number of texture slots 18. All textures will deleted")
+                for i, slot in enumerate(mat.texture_slots):
+                    mat.texture_slots.clear(i)
+
+        # Добавляем материал в слот материалов
+        print("Material slots number: ", len(md.materials))
+
+        if md.materials:
+            md.materials[0] = mat
+        else:
+            md.materials.append(mat)
+
+        # Задаем параметры диффузного цвета
+        if mesh.diffuse_color:
+            mat.diffuse_color = (
+            mesh.diffuse_color[0] / 255.0, mesh.diffuse_color[1] / 255.0, mesh.diffuse_color[2] / 255.0)
+
+        # Задаем параметры альфа-канала
+        if len(mesh.diffuse_color) > 3:
+            mat.alpha = float(mesh.diffuse_color[3]) / 255.0
+            mat.use_transparency = True
+            mat.transparency_method = 'Z_TRANSPARENCY'
+
+        # Настраиваем текстуру
+        if mesh.texture_file != "":
+            # Получаем имя каталога модели
+            modelDir = os.path.dirname(self.filepath)
+            # Вычисляем абсолютный путь к файлу текстуры
+            texImgPath = os.path.join(modelDir, mesh.texture_file)
+            # Грузим текстуру
+            img = bpy.data.images.load(texImgPath)
+
+            # Создаем новую текустуру
+            tex = bpy.data.textures.new("tex-" + mat.name, 'IMAGE')
+            tex.image = img
+
+            print("Textures slots: ", len(mat.texture_slots))
+
+            slot = mat.texture_slots.add()
+            slot.texture = tex
+            slot.texture_coords = 'UV'
+            slot.uv_layer = 'default'
+
+    # ---------------------------------------------------------------------------
+    #
+    # ---------------------------------------------------------------------------
+    def setUVcoords(self, md, mesh):
+        bm = bmesh.new()
+        bm.from_mesh(md)
+
+        uv_layer = bm.loops.layers.uv.new()
+
+        for f in bm.faces:
+            for v, l in zip(f.verts, f.loops):
+                luv = l[uv_layer]
+                print(v.index)
+                try:
+                    tmp = mesh.texcoords_list[v.index]
+                    print(tmp)
+
+                    luv.uv[0] = tmp[1]
+                    luv.uv[1] = tmp[2]
+
+                except Exception as ex:
+                    pass
+
+        bm.to_mesh(md)
+
+    #---------------------------------------------------------------------------
+    #
+    #---------------------------------------------------------------------------
+    def modelImport(self, path, is_coord_transform):
+
+        from . import CSV
+        from . import GeometryMath as gm
+
+        self.filepath = path
+
+        # Загружаем структуру модели из CSV-файла
+        loader = CSV.CSVLoader()
+        meshes_list = loader.loadCSV(path, is_coord_transform)
+
+        print("Loaded " + str(len(meshes_list)) + " meshes")
+
+        # Создаем все объекты, в соответсвии со списком мешей, полученом из CSV
+        for m_idx, m in enumerate(meshes_list):
+
+            # Берем имя объекта
+            obj_name = self.getFileName(path)
+
+            # Создаем меш объекта, в соответствии с загруженной геометрией
+            md = bpy.data.meshes.new(obj_name + "-" + str(m_idx))
+            md.from_pydata(m.vertex_list, [], m.faces_list)
+            md.update(calc_edges=True)
+
+            # Создаем материал
+            self.createMaterial(md, m)
+
+            # Создаем UV-развертку
+            self.setUVcoords(md, m)
+
+            # Если надо, выполняем трансформацию в правую СК
+            if is_coord_transform:
+                gm.toRightBasis(md)
+
+            # Содаем объект и добавляем его в сцену
+            obj = bpy.data.objects.new(md.name, md)
+            bpy.context.scene.objects.link(obj)
--- io_scene_csv/__init__.py
+++ io_scene_csv/__init__.py
@@ -15,85 +15,6 @@ bl_info = {
 }
 
 import bpy
-import bmesh
-import os
-
-def debug_print(f, msg):
-    f.write(msg + "\n")
-
-#-------------------------------------------------------------------------------
-#
-#-------------------------------------------------------------------------------
-def toRightBasis(md):
-    import mathutils
-    import math
-    mat_rot = mathutils.Matrix.Rotation(math.radians(90.0), 4, 'X')
-    mat_mirror_y = mathutils.Matrix()
-    mat_mirror_y[0][0], mat_mirror_y[0][1], mat_mirror_y[0][2], mat_mirror_y[0][3] = 1, 0, 0, 0
-    mat_mirror_y[1][0], mat_mirror_y[1][1], mat_mirror_y[1][2], mat_mirror_y[1][3] = 0, -1, 0, 0
-    mat_mirror_y[2][0], mat_mirror_y[2][1], mat_mirror_y[2][2], mat_mirror_y[2][3] = 0, 0, 1, 0
-    mat_mirror_y[3][0], mat_mirror_y[3][1], mat_mirror_y[3][2], mat_mirror_y[3][3] = 0, 0, 0, 1
-
-    for v in md.vertices:
-        v.co = mat_mirror_y * mat_rot * v.co
-
-#-------------------------------------------------------------------------------
-#
-#-------------------------------------------------------------------------------
-def invertVertexOrder(face):
-    new_face = []
-    new_face.append(face[0])
-
-    for i in range(len(face) - 1, 0, -1):
-        new_face.append(face[i])
-
-    return new_face
-
-#-------------------------------------------------------------------------------
-#
-#-------------------------------------------------------------------------------
-def toLeftBasis(obj, mesh):
-
-    import mathutils
-    import math
-
-    # Матрица поворота вокруг оси X
-    mat_rot = mathutils.Matrix.Rotation(math.radians(-90.0), 4, 'X')
-
-    # Матрица отражения вдоль оси Y
-    mat_mirror_y = mathutils.Matrix()
-    mat_mirror_y[0][0], mat_mirror_y[0][1], mat_mirror_y[0][2], mat_mirror_y[0][3] = 1, 0, 0, 0
-    mat_mirror_y[1][0], mat_mirror_y[1][1], mat_mirror_y[1][2], mat_mirror_y[1][3] = 0, -1, 0, 0
-    mat_mirror_y[2][0], mat_mirror_y[2][1], mat_mirror_y[2][2], mat_mirror_y[2][3] = 0, 0, 1, 0
-    mat_mirror_y[3][0], mat_mirror_y[3][1], mat_mirror_y[3][2], mat_mirror_y[3][3] = 0, 0, 0, 1
-
-    # Матрица перехода к мировым координатам
-    mat_world = obj.matrix_world
-
-    # Выделяем мтарицу поворота для мировых осей
-    loc, world_rot, scale = obj.matrix_world.decompose()
-
-    # Для каждой вершины
-    for i, vertex in enumerate(mesh.vertex_list):
-
-        loc_vert = mathutils.Vector(vertex)
-        loc_norm = mathutils.Vector(mesh.normals_list[i])
-
-        # Переходим к мировым координатам
-        world_vert = mat_world * loc_vert
-        # Переходим к левой системе координат
-        new_vert = mat_rot * mat_mirror_y * world_vert
-        mesh.vertex_list[i] = list(new_vert)
-
-        # Поворачиваем нормаль
-        world_norm = world_rot * loc_norm
-        new_norm = mat_rot * mat_mirror_y * world_norm
-        mesh.normals_list[i] = list(new_norm)
-
-    # Перелицовываем грани
-    for i, face in enumerate(mesh.faces_list):
-        mesh.faces_list[i] = invertVertexOrder(face)
-
 
 #-------------------------------------------------------------------------------
 #
@@ -113,108 +34,6 @@ class CSVImporter(bpy.types.Operator):
     )
 
     #---------------------------------------------------------------------------
-    #
-    #---------------------------------------------------------------------------
-    def getFileName(self, path):
-        basename = os.path.basename(path)
-        tmp = basename.split(".")
-        return tmp[0]
-
-    #---------------------------------------------------------------------------
-    #
-    #---------------------------------------------------------------------------
-    def createMaterial(self, md, mesh):
-
-        # Создаем материал. Если есть текстура, то используем имя
-        # файла текстуры в качестве имени материала
-        if mesh.texture_file != "":
-            matName = self.getFileName(mesh.texture_file)
-        else:
-            matName = md.name;
-
-        print("Creation of material: ", matName)
-
-        # Проверяем, существует ли уже такой материал
-        mat = bpy.data.materials.get(matName)
-
-        # Если не существует, создаем его заново
-        if mat is None:
-            mat = bpy.data.materials.new(name=matName)
-            print("New material ", matName, " created")
-        else:
-            print("Material ", matName, " already exists")
-            # проверяем число текстурных слотов в материале
-            if len(mat.texture_slots) == 18:
-                print("WARNING: maximal number of texture slots 18. All textures will deleted")
-                for i, slot in enumerate(mat.texture_slots):
-                    mat.texture_slots.clear(i)
-
-        # Добавляем материал в слот материалов
-        print("Material slots number: ", len(md.materials))
-
-        if md.materials:
-            md.materials[0] = mat
-        else:
-            md.materials.append(mat)
-
-        # Задаем параметры диффузного цвета
-        if mesh.diffuse_color:
-            mat.diffuse_color = (mesh.diffuse_color[0] / 255.0, mesh.diffuse_color[1] / 255.0, mesh.diffuse_color[2] / 255.0)
-
-        # Задаем параметры альфа-канала
-        if len(mesh.diffuse_color) > 3:
-            mat.alpha = float(mesh.diffuse_color[3]) / 255.0
-            mat.use_transparency = True
-            mat.transparency_method = 'Z_TRANSPARENCY'
-
-        # Настраиваем текстуру
-        if mesh.texture_file != "":
-
-            # Получаем имя каталога модели
-            modelDir = os.path.dirname(self.filepath)
-            # Вычисляем абсолютный путь к файлу текстуры
-            texImgPath = os.path.join(modelDir, mesh.texture_file)
-            # Грузим текстуру
-            img = bpy.data.images.load(texImgPath)
-
-            # Создаем новую текустуру
-            tex = bpy.data.textures.new("tex-" + mat.name, 'IMAGE')
-            tex.image = img
-
-            print("Textures slots: ", len(mat.texture_slots))
-
-            slot = mat.texture_slots.add()
-            slot.texture = tex
-            slot.texture_coords = 'UV'
-            slot.uv_layer = 'default'
-
-    #---------------------------------------------------------------------------
-    #
-    #---------------------------------------------------------------------------
-    def setUVcoords(self, md, mesh):
-        bm = bmesh.new()
-        bm.from_mesh(md)
-
-        uv_layer = bm.loops.layers.uv.new()
-
-        for f in bm.faces:
-            for v, l in zip(f.verts, f.loops):
-                luv = l[uv_layer]
-                print(v.index)
-                try:
-                    tmp = mesh.texcoords_list[v.index]
-                    print(tmp)
-
-                    luv.uv[0] = tmp[1]
-                    luv.uv[1] = tmp[2]
-
-                except Exception as ex:
-                    pass
-
-        bm.to_mesh(md)
-
-
-    #---------------------------------------------------------------------------
     # Action by menu item selection
     #---------------------------------------------------------------------------
     def execute(self, context):
@@ -222,42 +41,16 @@ class CSVImporter(bpy.types.Operator):
         path = self.filepath
         print("Loading model from file: " + path)
 
-        from . import CSV
+        from . import ImportCSV
+        importer = ImportCSV.ImportCSV()
 
-        # Загружаем структуру модели из CSV-файла
-        loader = CSV.CSVLoader()
-        meshes_list = loader.loadCSV(path, self.use_left_coords_transform)
-
-        print("Loaded " + str(len(meshes_list)) + " meshes")
-
-        # Создаем все объекты, в соответсвии со списком мешей, полученом из CSV
-        for m_idx, m in enumerate(meshes_list):
-
-            # Берем имя объекта
-            obj_name = self.getFileName(path)
-
-            # Создаем меш объекта, в соответствии с загруженной геометрией
-            md = bpy.data.meshes.new(obj_name + "-" + str(m_idx))
-            md.from_pydata(m.vertex_list, [], m.faces_list)
-            md.update(calc_edges=True)
-
-            # Создаем материал
-            self.createMaterial(md, m)
-
-            # Создаем UV-развертку
-            self.setUVcoords(md, m)
-
-            # Если надо, выполняем трансформацию в правую СК
-            if self.use_left_coords_transform:
-                toRightBasis(md)
-
-            # Содаем объект и добавляем его в сцену
-            obj = bpy.data.objects.new(md.name, md)
-            bpy.context.scene.objects.link(obj)
+        importer.modelImport(path, self.use_left_coords_transform)
 
         return {'FINISHED'}
 
+    #---------------------------------------------------------------------------
     # Select file for import
+    #---------------------------------------------------------------------------
     def invoke(self, context, event):
         context.window_manager.fileselect_add(self)
         return {'RUNNING_MODAL'}
